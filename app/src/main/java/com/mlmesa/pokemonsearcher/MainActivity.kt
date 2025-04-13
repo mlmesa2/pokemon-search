da5230aa39ecac6c6ecf9360d947ca84e5051a8e
mlmesa <mlmesa@protonmail.com>
2025-04-13 00:12:08 -0600

    feat: Integrate Hilt for dependency injection
    This commit integrates Hilt for dependency injection in the project. It includes:
    
    - Adding Hilt plugins and dependencies
    - Other hilt's configurations

--- app/src/main/java/com/mlmesa/pokemonsearcher/MainActivity.kt
+++ app/src/main/java/com/mlmesa/pokemonsearcher/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mlmesa.pokemonsearcher.ui.theme.PokemonSearcherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
